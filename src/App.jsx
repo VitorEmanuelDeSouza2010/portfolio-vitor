9481790b7ffda7c9da37e8be96a791c6f27c7033
Vitor Emanuel de Souza <vitorlego321@gmail.com>
2025-10-30 10:03:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css'
+
+function App() {
+
+  //javascript
+
+  return (
+    <>
+    <h1 className={styles.titulo}>nada</h1>
+    </>
+  )
+}
+
+export default App
